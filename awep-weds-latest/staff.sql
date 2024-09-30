81312794ebe6bac5836f2cca7e2fe38b41b99199
amirahnana <23FTT1184@student.pb.edu.bn>
2024-09-30 09:38:17 +0800

    Delete awep-weds-latest directory

--- awep-weds-latest/staff.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE staff (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    role ENUM('admin', 'bride', 'groom') NOT NULL
-);
